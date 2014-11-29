b8e9775059323e836d8bb210976a8d7a53015e02
Inderjit Gill <email@indy.io>
2014-11-29 14:06:52 +0000

    replaced Lexer class with exported functions

--- src/lang/lexer.js
+++ src/lang/lexer.js
@@ -3,238 +3,235 @@ import {
   TokenType
 } from './token';
 
-export class Lexer {
+export function tokenise(input) {
+  let q = [],                 // queue of tokens to return
+      p = [];                 // [token, remaining] pair
 
-  get MINUS()  { return '-'; }
-  get PERIOD() { return '.'; }
+  let s = skipWhitespace(input);
 
-  tokenise(input) {
-    let q = [],                 // queue of tokens to return
-        p = [];                 // [token, remaining] pair
+  while(s.length > 0) {
+    switch(nextTokenType(s)) {
+    case TokenType.LIST_START :
+      p = consumeListStart(s);
+      break;
+    case TokenType.LIST_END :
+      p = consumeListEnd(s);
+      break;
+    case TokenType.BRACKET_START :
+      p = consumeBracketStart(s);
+      break;
+    case TokenType.BRACKET_END :
+      p = consumeBracketEnd(s);
+      break;
+    case TokenType.STRING :
+      p = consumeString(s);
+      break;
+    case TokenType.NAME :
+      p = consumeName(s);
+      break;
+    case TokenType.INT :
+      p = consumeInt(s);
+      break;
+    case TokenType.FLOAT :
+      p = consumeFloat(s);
+      break;
+    case TokenType.QUOTE_ABBREVIATION :
+      p = consumeQuoteAbbreviation(s);
+      break;
+      // todo: add a default that throws an exception?
+      // todo: or just add an UNKNOWN token to q
+    };
 
-    let s = this.skipWhitespace(input);
-
-    while(s.length > 0) {
-      switch(this.nextTokenType(s)) {
-      case TokenType.LIST_START :
-        p = this.consumeListStart(s);
-        break;
-      case TokenType.LIST_END :
-        p = this.consumeListEnd(s);
-        break;
-      case TokenType.BRACKET_START :
-        p = this.consumeBracketStart(s);
-        break;
-      case TokenType.BRACKET_END :
-        p = this.consumeBracketEnd(s);
-        break;
-      case TokenType.STRING :
-        p = this.consumeString(s);
-        break;
-      case TokenType.NAME :
-        p = this.consumeName(s);
-        break;
-      case TokenType.INT :
-        p = this.consumeInt(s);
-        break;
-      case TokenType.FLOAT :
-        p = this.consumeFloat(s);
-        break;
-      case TokenType.QUOTE_ABBREVIATION :
-        p = this.consumeQuoteAbbreviation(s);
-        break;
-        // todo: add a default that throws an exception?
-        // todo: or just add an UNKNOWN token to q
-      };
-
-      let [token, remaining] = p;
-      
-      q.push(token);
-      s = this.skipWhitespace(remaining);
-    }
-
-    return q;
-  }
-
-  skipWhitespace(s) {
-    for(let i=0;i<s.length;i++) {
-      if(!this.isWhitespace(s[i])) {
-        return s.substring(i);
-      }
-    }
-    return "";
-  }
-
-  consumeInt(s) {
-    let i = 0;
-    for(i=0;i<s.length;i++) {
-      let c = s[i];
-      if(!this.isDigit(c) && c != this.MINUS) {
-        break;
-      }
-
-      if(!this.isDigit(c) && !(i === 0 && c === this.MINUS)) {
-        break;
-      }
-    }
+    let [token, remaining] = p;
     
-    let token = new Token(TokenType.INT, parseInt(s.substring(0, i)));
-    return [token, s.substring(i, s.length)];
+    q.push(token);
+    s = skipWhitespace(remaining);
   }
 
-  consumeFloat(s) {
-    let i = 0;
-    for(i=0;i<s.length;i++) {
-      let c = s[i];
-      if(!this.isDigit(c) && !(i===0 && c === this.MINUS) && c != this.PERIOD) {
-        break;
-      }
+  return q;
+}
+
+let MINUS = '-';
+let PERIOD = '.';
+
+export function skipWhitespace(s) {
+  for(let i=0;i<s.length;i++) {
+    if(!isWhitespace(s[i])) {
+      return s.substring(i);
     }
-    
-    let token = new Token(TokenType.FLOAT, parseFloat(s.substring(0, i)));
-    return [token, s.substring(i, s.length)];
   }
+  return "";
+}
 
-  consumeListStart(s) {
-    return [new Token(TokenType.LIST_START), s.substring(1)];
-  }
+export function consumeInt(s) {
+  let i = 0;
+  for(i=0;i<s.length;i++) {
+    let c = s[i];
+    if(!isDigit(c) && c != MINUS) {
+      break;
+    }
 
-  consumeListEnd(s) {
-    return [new Token(TokenType.LIST_END), s.substring(1)];
-  }
-
-  consumeBracketStart(s) {
-    return [new Token(TokenType.BRACKET_START), s.substring(1)];
-  }
-
-  consumeBracketEnd(s) {
-    return [new Token(TokenType.BRACKET_END), s.substring(1)];
-  }
-
-  consumeString(s) {
-    let val = s.substring(1); // skip first \"
-    let nextQuote = val.indexOf('\"');
-    val = val.substring(0, nextQuote);
-
-    let token = new Token(TokenType.STRING, val);
-    return [token, s.substring(nextQuote + 2)];
+    if(!isDigit(c) && !(i === 0 && c === MINUS)) {
+      break;
+    }
   }
   
-  consumeName(s) {
-    let i=0;
-    for(i=0;i<s.length;i++) {
-      let c = s[i];
-      if(!this.isAlpha(c) && !this.isDigit(c) && !this.isSymbol(c)) {
-        break;
-      }
+  let token = new Token(TokenType.INT, parseInt(s.substring(0, i)));
+  return [token, s.substring(i, s.length)];
+}
+
+export function consumeFloat(s) {
+  let i = 0;
+  for(i=0;i<s.length;i++) {
+    let c = s[i];
+    if(!isDigit(c) && !(i===0 && c === MINUS) && c != PERIOD) {
+      break;
     }
-    let token = new Token(TokenType.NAME, s.substring(0, i));
-    return [token, s.substring(i, s.length)];
   }
   
-  consumeQuoteAbbreviation(s) {
-    return [new Token(TokenType.QUOTE_ABBREVIATION), s.substring(1)];
-  }
+  let token = new Token(TokenType.FLOAT, parseFloat(s.substring(0, i)));
+  return [token, s.substring(i, s.length)];
+}
 
-  nextTokenType(s) {
-    let c = s[0];
+export function consumeListStart(s) {
+  return [new Token(TokenType.LIST_START), s.substring(1)];
+}
 
-    if(this.isQuoteAbbreviation(c)) {
-      return TokenType.QUOTE_ABBREVIATION;
+export function consumeListEnd(s) {
+  return [new Token(TokenType.LIST_END), s.substring(1)];
+}
+
+export function consumeBracketStart(s) {
+  return [new Token(TokenType.BRACKET_START), s.substring(1)];
+}
+
+export function consumeBracketEnd(s) {
+  return [new Token(TokenType.BRACKET_END), s.substring(1)];
+}
+
+export function consumeString(s) {
+  let val = s.substring(1); // skip first \"
+  let nextQuote = val.indexOf('\"');
+  val = val.substring(0, nextQuote);
+
+  let token = new Token(TokenType.STRING, val);
+  return [token, s.substring(nextQuote + 2)];
+}
+
+export function consumeName(s) {
+  let i=0;
+  for(i=0;i<s.length;i++) {
+    let c = s[i];
+    if(!isAlpha(c) && !isDigit(c) && !isSymbol(c)) {
+      break;
     }
+  }
+  let token = new Token(TokenType.NAME, s.substring(0, i));
+  return [token, s.substring(i, s.length)];
+}
 
-    if(this.isListStart(c)) {
-      return TokenType.LIST_START;
+export function consumeQuoteAbbreviation(s) {
+  return [new Token(TokenType.QUOTE_ABBREVIATION), s.substring(1)];
+}
+
+export function nextTokenType(s) {
+  let c = s[0];
+
+  if(isQuoteAbbreviation(c)) {
+    return TokenType.QUOTE_ABBREVIATION;
+  }
+
+  if(isListStart(c)) {
+    return TokenType.LIST_START;
+  }
+
+  if(isListEnd(c)) {
+    return TokenType.LIST_END;
+  }
+
+  if(isBracketStart(c)) {
+    return TokenType.BRACKET_START;
+  }
+
+  if(isBracketEnd(c)) {
+    return TokenType.BRACKET_END;
+  }
+
+  if(isQuotedString(c)) {
+    return TokenType.STRING;
+  }
+
+  if(isName(c)) {
+    if(c === MINUS && s.length > 1 && isDigit(s[1])) {
+      // don't treat negative numbers as NAMEs
+    } else {
+      return TokenType.NAME;
     }
+  }
 
-    if(this.isListEnd(c)) {
-      return TokenType.LIST_END;
+  if(isDigit(c) || c === MINUS || c === PERIOD) {
+    return hasPeriod(s) ? TokenType.FLOAT : TokenType.INT;
+  }
+
+  return TokenType.UNKNOWN;
+}
+
+// is there a period in the stream of characters before we get to whitespace
+function hasPeriod(s) {
+  for(let i=0; i<s.length; i++) {
+    if(s[i] === PERIOD) {
+      return true;
     }
-
-    if(this.isBracketStart(c)) {
-      return TokenType.BRACKET_START;
+    if(isWhitespace(s[i])) {
+      return false;
     }
-
-    if(this.isBracketEnd(c)) {
-      return TokenType.BRACKET_END;
-    }
-
-    if(this.isQuotedString(c)) {
-      return TokenType.STRING;
-    }
-
-    if(this.isName(c)) {
-      if(c === this.MINUS && s.length > 1 && this.isDigit(s[1])) {
-        // don't treat negative numbers as NAMEs
-      } else {
-        return TokenType.NAME;
-      }
-    }
-
-    if(this.isDigit(c) || c === this.MINUS || c === this.PERIOD) {
-      return this.hasPeriod(s) ? TokenType.FLOAT : TokenType.INT;
-    }
-
-    return TokenType.UNKNOWN;
   }
+  return false;
+}
 
-  // is there a period in the stream of characters before we get to whitespace
-  hasPeriod(s) {
-    for(let i=0; i<s.length; i++) {
-      if(s[i] === this.PERIOD) {
-        return true;
-      }
-      if(this.isWhitespace(s[i])) {
-        return false;
-      }
-    }
-    return false;
-  }
+function isWhitespace(character) {
+  return sWhitespaceSet.has(character);
+}
 
-  isWhitespace(character) {
-    return sWhitespaceSet.has(character);
-  }
+function isDigit(character) {
+  return sDigitSet.has(character);
+}
 
-  isDigit(character) {
-    return sDigitSet.has(character);
-  }
+function isAlpha(character) {
+  return sAlphaSet.has(character);
+}
 
-  isAlpha(character) {
-    return sAlphaSet.has(character);
-  }
+function isSymbol(character) {
+  return sSymbolSet.has(character);
+}
 
-  isSymbol(character) {
-    return sSymbolSet.has(character);
-  }
+function isListStart(character) {
+  return character === '(';
+}
 
-  isListStart(character) {
-    return character === '(';
-  }
+function isListEnd(character) {
+  return character === ')';
+}
 
-  isListEnd(character) {
-    return character === ')';
-  }
+function isBracketStart(character) {
+  return character === '[';
+}
 
-  isBracketStart(character) {
-    return character === '[';
-  }
+function isBracketEnd(character) {
+  return character === ']';
+}
 
-  isBracketEnd(character) {
-    return character === ']';
-  }
+function isQuotedString(character) {
+  return character === '"';
+}
 
-  isQuotedString(character) {
-    return character === '"';
-  }
-  
-  isQuoteAbbreviation(character) {
-    return character === '\'';
-  }
+function isQuoteAbbreviation(character) {
+  return character === '\'';
+}
 
-  isName(character) {
-    return this.isAlpha(character);
-  }
-};
+function isName(character) {
+  return isAlpha(character);
+}
 
 var sWhitespaceSet = characterSet(" \t\n");
 var sDigitSet = characterSet("0123456789");
--- test/lang/lexer.spec.js
+++ test/lang/lexer.spec.js
@@ -1,5 +1,16 @@
 import {
-  Lexer
+  tokenise,
+  skipWhitespace,
+  nextTokenType,
+  consumeInt,
+  consumeFloat,
+  consumeListStart,
+  consumeListEnd,
+  consumeBracketStart,
+  consumeBracketEnd,
+  consumeString,
+  consumeName,
+  consumeQuoteAbbreviation
 } from '../../src/lang/lexer';
 
 import {
@@ -8,14 +19,9 @@ import {
 } from '../../src/lang/token';
 
 describe('Lexer', function () {
-  var lexer;
-
-  beforeEach(function () {
-    lexer = new Lexer();
-  });
 
   it('should tokenise strings', function() {
-    let q = lexer.tokenise("(go 42 3.14)");
+    let q = tokenise("(go 42 3.14)");
     expect(q.length).toEqual(5);
     expect(q[0].getType()).toEqual(TokenType.LIST_START);
     expect(q[1].getType()).toEqual(TokenType.NAME);
@@ -23,7 +29,7 @@ describe('Lexer', function () {
     expect(q[3].getType()).toEqual(TokenType.FLOAT);
     expect(q[4].getType()).toEqual(TokenType.LIST_END);
 
-    q = lexer.tokenise("(go [\"hi\"] 'SOMETHING)");
+    q = tokenise("(go [\"hi\"] 'SOMETHING)");
     expect(q.length).toEqual(8);
     expect(q[0].getType()).toEqual(TokenType.LIST_START);
     expect(q[1].getType()).toEqual(TokenType.NAME);
@@ -35,70 +41,65 @@ describe('Lexer', function () {
     expect(q[7].getType()).toEqual(TokenType.LIST_END);
 });
 
-  it('should detect whitespace', function () {
-    expect(lexer.isWhitespace(' ')).toBe(true);
-    expect(lexer.isWhitespace('d')).not.toBe(true);
-  });
-
   it('should skip whitespace', function () {
-    expect(lexer.skipWhitespace('hello')).toEqual("hello");
-    expect(lexer.skipWhitespace('     hello')).toEqual("hello");
-    expect(lexer.skipWhitespace('\t\thello')).toEqual("hello");
+    expect(skipWhitespace('hello')).toEqual("hello");
+    expect(skipWhitespace('     hello')).toEqual("hello");
+    expect(skipWhitespace('\t\thello')).toEqual("hello");
   });
 
   it('should get the nextTokenType', function() {
-    expect(lexer.nextTokenType("'FOO")).toEqual(TokenType.QUOTE_ABBREVIATION);
-    expect(lexer.nextTokenType("(FOO")).toEqual(TokenType.LIST_START);
-    expect(lexer.nextTokenType(") ")).toEqual(TokenType.LIST_END);
-    expect(lexer.nextTokenType("[45")).toEqual(TokenType.BRACKET_START);
-    expect(lexer.nextTokenType("]")).toEqual(TokenType.BRACKET_END);
-    expect(lexer.nextTokenType("\"hello\"")).toEqual(TokenType.STRING);
-    expect(lexer.nextTokenType("BAR")).toEqual(TokenType.NAME);
-    expect(lexer.nextTokenType("42")).toEqual(TokenType.INT);
-    expect(lexer.nextTokenType("42.0")).toEqual(TokenType.FLOAT);
-    expect(lexer.nextTokenType("-42")).toEqual(TokenType.INT);
-    expect(lexer.nextTokenType("-42.0")).toEqual(TokenType.FLOAT);
-    expect(lexer.nextTokenType(".0123")).toEqual(TokenType.FLOAT);
+    expect(nextTokenType("'FOO")).toEqual(TokenType.QUOTE_ABBREVIATION);
+    expect(nextTokenType("(FOO")).toEqual(TokenType.LIST_START);
+    expect(nextTokenType(") ")).toEqual(TokenType.LIST_END);
+    expect(nextTokenType("[45")).toEqual(TokenType.BRACKET_START);
+    expect(nextTokenType("]")).toEqual(TokenType.BRACKET_END);
+    expect(nextTokenType("\"hello\"")).toEqual(TokenType.STRING);
+    expect(nextTokenType("BAR")).toEqual(TokenType.NAME);
+    expect(nextTokenType("42")).toEqual(TokenType.INT);
+    expect(nextTokenType("42.0")).toEqual(TokenType.FLOAT);
+    expect(nextTokenType("-42")).toEqual(TokenType.INT);
+    expect(nextTokenType("-42.0")).toEqual(TokenType.FLOAT);
+    expect(nextTokenType(".0123")).toEqual(TokenType.FLOAT);
   });
 
 
   it('should consume an int', function() {
-    let [token, rem] = lexer.consumeInt("999 remaining");
+    let [token, rem] = consumeInt("999 remaining");
     expect(token.getType()).toEqual(TokenType.INT);
     expect(token.getValue()).toEqual(999);
     expect(rem).toEqual(" remaining");
   });
 
   it('should consume a float', function() {
-    let [token, rem] = lexer.consumeFloat("43.21 remaining");
+    let [token, rem] = consumeFloat("43.21 remaining");
     expect(token.getType()).toEqual(TokenType.FLOAT);
     expect(token.getValue()).toBeCloseTo(43.21);
     expect(rem).toEqual(" remaining");
   });
 
   it('should consume a starting list', function() {
-    let [token, rem] = lexer.consumeListStart("(remaining");
+    let [token, rem] = consumeListStart("(remaining");
     expect(token.getType()).toEqual(TokenType.LIST_START);
     expect(token.getValue()).toEqual(undefined);
     expect(rem).toEqual("remaining");
   });
 
   it('should consume an end list', function() {
-    let [token, rem] = lexer.consumeListEnd(") remaining");
+    let [token, rem] = consumeListEnd(") remaining");
     expect(token.getType()).toEqual(TokenType.LIST_END);
     expect(token.getValue()).toEqual(undefined);
     expect(rem).toEqual(" remaining");
   });
 
   it('should consume a starting bracket', function() {
-    let [token, rem] = lexer.consumeBracketStart("[remaining");
+    let [token, rem] = consumeBracketStart("[remaining");
     expect(token.getType()).toEqual(TokenType.BRACKET_START);
     expect(token.getValue()).toEqual(undefined);
     expect(rem).toEqual("remaining");
   });
 
   it('should consume an end bracket', function() {
-    let [token, rem] = lexer.consumeBracketEnd("] remaining");
+    let [token, rem] = consumeBracketEnd("] remaining");
     expect(token.getType()).toEqual(TokenType.BRACKET_END);
     expect(token.getValue()).toEqual(undefined);
     expect(rem).toEqual(" remaining");
@@ -106,21 +107,21 @@ describe('Lexer', function () {
 
   
   it('should consume a string', function() {
-    let [token, rem] = lexer.consumeString("\"string\" this is remaining");
+    let [token, rem] = consumeString("\"string\" this is remaining");
     expect(token.getType()).toEqual(TokenType.STRING);
     expect(token.getValue()).toEqual("string");
     expect(rem).toEqual(" this is remaining");
   });
 
   it('should consume a name', function() {
-    let [token, rem] = lexer.consumeName("NAME this is leftover");
+    let [token, rem] = consumeName("NAME this is leftover");
     expect(token.getType()).toEqual(TokenType.NAME);
     expect(token.getValue()).toEqual("NAME");
     expect(rem).toEqual(" this is leftover");
   });
 
   it('should consume a quote abbreviation', function() {
-    let [token, rem] = lexer.consumeQuoteAbbreviation("'QUOTEDNAME");
+    let [token, rem] = consumeQuoteAbbreviation("'QUOTEDNAME");
     expect(token.getType()).toEqual(TokenType.QUOTE_ABBREVIATION);
     expect(token.getValue()).toEqual(undefined);
     expect(rem).toEqual("QUOTEDNAME");
