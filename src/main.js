3ea136dd6604bb1b0fc71527911395af9dfc25f0
Inderjit Gill <email@indy.io>
2014-12-06 14:23:17 +0000

    latest polymer changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import { Renderer } from './Renderer';
 
 document.addEventListener('polymer-ready', function() {
-  var navicon = document.getElementById('navicon');
-  var drawerPanel = document.getElementById('drawerPanel');
-  navicon.addEventListener('click', function() {
-    drawerPanel.togglePanel();
-  });
-
   var renderer = new Renderer("render-canvas");
   renderer.preDrawScene();
   renderer.drawScene();
