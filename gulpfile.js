a0fe4530930f61e3e738bd35f10526db38d29cee
Inderjit Gill <email@indy.io>
2014-12-06 22:24:34 +0000

    removed amd build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
-    pipe = require('pipe/gulp'),
-    karma = require('./lib/gulp/karma'),
-    traceur = require('gulp-traceur'),
-    jshint = require('gulp-jshint');
+pipe = require('pipe/gulp'),
+karma = require('./lib/gulp/karma'),
+traceur = require('gulp-traceur'),
+jshint = require('gulp-jshint');
 
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
@@ -10,45 +10,38 @@ var source = require('vinyl-source-stream');
 var webserver = require('gulp-webserver');
 
 var paths = {
-  src: 'src/**/*.js'
+    src: 'src/**/*.js'
 };
 
 gulp.task('webserver', function() {
-  gulp.src('.')
-    .pipe(webserver({
-//      livereload: true,
-      directoryListing: true,
-//      open: true
-    }));
+    gulp.src('.')
+        .pipe(webserver({
+            //      livereload: true,
+            directoryListing: true,
+            //      open: true
+        }));
 });
 
 gulp.task('lint', function() {
-  gulp.src(paths.src)
-  .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(jshint.reporter('fail'));
-});
-
-gulp.task('build:amd', function() {
-  gulp.src(paths.src)
-    .pipe(traceur(pipe.traceur({experimental: true})))
-  .pipe(gulp.dest('dist/amd'));
+    gulp.src(paths.src)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('build:cjs', function() {
-  return gulp.src(paths.src)
-    .pipe(traceur(pipe.traceur({modules: 'commonjs',
-                                experimental: true})))
-    .pipe(gulp.dest('dist/cjs'));
+    return gulp.src(paths.src)
+        .pipe(traceur(pipe.traceur({modules: 'commonjs',
+                                    experimental: true})))
+        .pipe(gulp.dest('dist/cjs'));
 });
 
 gulp.task('build:es6', function() {
-  gulp.src(paths.src)
-    .pipe(traceur(pipe.traceur({outputLanguage: 'es6',
-                               experimental: true})))
-  .pipe(gulp.dest('dist/es6'));
+    gulp.src(paths.src)
+        .pipe(traceur(pipe.traceur({outputLanguage: 'es6',
+                                    experimental: true})))
+        .pipe(gulp.dest('dist/es6'));
 });
-
 
 gulp.task('browserify', ['build:cjs'], function() {
     return browserify('./dist/cjs/main.js')
@@ -58,18 +51,18 @@ gulp.task('browserify', ['build:cjs'], function() {
 });
 
 // todo: add lint as the first subtask of build
-gulp.task('build', ['build:amd', 'build:cjs', 'build:es6', 'browserify']);
+gulp.task('build', ['build:cjs', 'build:es6', 'browserify']);
 
 gulp.task('test', function(done) {
-  var options = {
-    configFile: 'karma.conf.js'
-  };
-  for (var i=0, ii = process.argv.length; i<ii; ++i) {
-    var val = process.argv[i];
-    if (val === '--debug') options.debugRun = true;
-    if (val === '--watch') options.autoWatch = true;
-    else if (val === '--single-run') options.singleRun = true;
-    else if (val === '--browsers') options.browsers = process.argv[++i].split(',');
-  }
-  karma(options, done);
+    var options = {
+        configFile: 'karma.conf.js'
+    };
+    for (var i=0, ii = process.argv.length; i<ii; ++i) {
+        var val = process.argv[i];
+        if (val === '--debug') options.debugRun = true;
+        if (val === '--watch') options.autoWatch = true;
+        else if (val === '--single-run') options.singleRun = true;
+        else if (val === '--browsers') options.browsers = process.argv[++i].split(',');
+    }
+    karma(options, done);
 });
