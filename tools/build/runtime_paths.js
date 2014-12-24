d07cdde93d5a95e5a8fdfa3121b0da004c22c9dd
Inderjit Gill <email@indy.io>
2014-12-24 12:56:41 +0000

    baseURL for runtime paths

--- tools/build/runtime_paths.js
+++ tools/build/runtime_paths.js
@@ -1,8 +1,10 @@
+System.baseURL = '/js/';
+
 System.paths = {
-  'seni/*': '/js/seni/lib/*.js',
-  'lang/*': '/js/lang/lib/*.js',
-  'facade/*': '/js/facade/lib/*.js',
-  'rtts_assert/*': '/js/rtts_assert/lib/*.js',
-  'test_lib/*': '/js/test_lib/lib/*.js'
+  'seni/*': 'seni/lib/*.js',
+  'lang/*': 'lang/lib/*.js',
+  'facade/*': 'facade/lib/*.js',
+  'rtts_assert/*': 'rtts_assert/lib/*.js',
+  'test_lib/*': 'test_lib/lib/*.js'
 };
 register(System);
