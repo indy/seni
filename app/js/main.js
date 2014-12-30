f5f4dd9cf3fae074d16d2fef5595e5b00b15b6bb
Inderjit Gill <email@indy.io>
2014-12-30 12:41:59 +0000

    basic textarea ui

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,30 @@ import { createEnv, evalForm } from 'lang/runtime';
 import * as seniBind from 'seni/bind';
 
 export function main() {
+
   var renderer = new Renderer("render-canvas");
+  setupUI(renderer);
+
+}
+
+
+function setupUI(renderer) {
+  let d = document;
+
+  let textArea = d.getElementById("textarea");
+  let autogrow = d.getElementById("autogrow-textarea");  
+
+  textArea.value = "(bezier 15 20 (list (list 440 400) (list 533 700) (list 766 200) (list 900 500)) 0 1)";
+  autogrow.update();
+
+  d.getElementById("my-button").addEventListener("click", (e) => {
+    renderScript(renderer, textArea.value);
+  });
+}
+
+function renderScript(renderer, form) {
   let env = createEnv();
   env = seniBind.addBindings(env, renderer);
-
-  let form = "(bezier 15 20 (list (list 440 400) (list 533 700) (list 766 200) (list 900 500)) 0 1)";
 
   renderer.preDrawScene();
   evalForm(env, form);
