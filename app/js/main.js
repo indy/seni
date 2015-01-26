13636acb4113a7c4009260ab02bfd23007c3d145
Inderjit Gill <email@indy.io>
2015-01-26 22:46:16 +0000

    render-canvas html width and height attributes

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,11 @@ import { createEnv, evalForm } from 'lang/runtime';
 import * as seniBind from 'seni/bind';
 
 export function main() {
-  var renderer = new Renderer("render-canvas");
-  setupUI(renderer);
+    var renderer = new Renderer("render-canvas");
+    setupUI(renderer);
 
-  // show something on the canvas at startup
-  renderScript(renderer, initialCode());
+    // show something on the canvas at startup
+    renderScript(renderer, initialCode());
 }
 
 function setupUI(renderer) {
@@ -39,38 +39,30 @@ function renderScript(renderer, form) {
 function initialCode() {
   return `
 
-(let ((c 50))
-  (loop (j from: (/ c 2) to: 1000 step: c)
-        (loop (i from: (/ c 2) to: 1000 step: c) 
-              (rect width: c height: c
-                    x: i y: j
-                    colour: (rgbColour r: (perlin) 
-                                       g: (perlin) 
-                                       b: (perlin)
-                             a: 0.5)))))
+
+
+(let ((c 30) (m 2.2))
+  (loop (j from: (/ c 2) to: 1100 step: c)
+        (loop (i from: (/ c 2) to: 1100 step: c) 
+              (pushMatrix) 
+               (translate x: i y: j)
+               (rotate angle: (/ 3.14 4.1))
+               (rect width: (* c m) height: (* c m)
+                     colour: (rgbColour r: (perlin) 
+                                        g: (perlin) 
+                                        b: (perlin)
+                                        a: 0.7))
+              
+             (popMatrix) 
+              )))
+
 `;
 }
 
 
 /*
 
-(let ((c 50))
-  (loop (j from: (/ c 2) to: 1000 step: c)
-        (loop (i from: (/ c 2) to: 1000 step: c) 
-              (rect width: c height: c
-                    x: i y: j
-                    colour: (rgbColour r: (perlin) 
-                                       g: (perlin) 
-                                       b: (perlin)
-                             a: 0.5)))))
 
-
- 
-
-(rect x: 500 y: 500 width: 200 height: 200 
-      colour: (rgbColour r: (perlin) 
-                         g: (perlin) 
-                         b: (perlin)))
 (scale x: 2.5 y: 2.5)
 (rotate angle: (/ 3.14 4))
 (translate x: 300)
@@ -92,27 +84,6 @@ function initialCode() {
                            b: 1.0
                            a: 0.4))
 
-
-
-
-
-
-
-
-
-
-(scale x: 2.5 y: 2.5)
-(rotate angle: (/ 3.14 4))
-(translate x: 300)
-(rect width: 200 height: 200 
-      colour: (rgbColour r: (perlin) 
-                         g: (perlin) 
-                         b: (perlin)
-                         a: 0.5))
-
-
-
-
 (rect x: 500 y: 500 width: 200 height: 200 colour: (rgbColour r: (perlin) g: (perlin) b: (perlin)))
 (scale x: 2.5 y: 2.5)
 (rotate angle: (/ 3.14 4))
@@ -131,32 +102,39 @@ function initialCode() {
 
 
 
-(let ((c 50) (m 1.2))
-  (loop (j from: (/ c 2) to: 1000 step: c)
-        (loop (i from: (/ c 2) to: 1000 step: c) 
-              (pushMatrix)
-              (translate x: i y: j)
-              (rotate angle: (/ 3.14 4))
-              (rect width: (* c m) height: (* c m)
-                    colour: (rgbColour r: (perlin) 
-                                       g: (perlin) 
-                                       b: (perlin)
-                             a: 0.7))
-              (popMatrix))))
-
-
-                
-(let ((c 50) (m 1.2))
-  (loop (j from: (/ c 2) to: 1000 step: c)
-        (loop (i from: (/ c 2) to: 1000 step: c) 
+// a possible wash background:
+(let ((c 30) (m 2.2))
+  (loop (j from: (/ c 2) to: 1100 step: c)
+        (loop (i from: (/ c 2) to: 1100 step: c) 
               (pushMatrix) 
                (translate x: i y: j)
-               (rotate angle: (/ 3.14 4))
+               (rotate angle: (/ 3.14 0.29))
                (rect width: (* c m) height: (* c m)
                      colour: (rgbColour r: (perlin) 
                                         g: (perlin) 
                                         b: (perlin)
-                                        a: 0.7))
+                                        a: 0.3))
               
              (popMatrix) 
-              )))                */
+              )))
+
+
+// another good wash background:
+(let ((c 30) (m 2.2))
+  (loop (j from: (/ c 2) to: 1100 step: c)
+        (loop (i from: (/ c 2) to: 1100 step: c) 
+              (pushMatrix) 
+               (translate x: j y: i)
+               (rotate angle: (/ 3.14 (+ 0.26 (* 0.2 (perlin)))))
+               (rect width: (* c m) height: (* c m)
+                     colour: (rgbColour r: (+ 0.5 (* 0.1 (perlin))) 
+                                        g: (+ 0.5 (* 0.1 (perlin))) 
+                                        b: (+ 0.5 (* 0.3 (perlin)))
+                                        a: 0.9))
+              
+             (popMatrix) 
+              )))
+
+
+
+*/
