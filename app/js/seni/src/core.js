374536008a97e3dc494978f29b340e49e42200ff
Inderjit Gill <email@indy.io>
2015-02-14 13:19:06 +0000

    take doc

--- app/js/seni/src/core.js
+++ app/js/seni/src/core.js
@@ -2,7 +2,9 @@ import { PublicBinding } from 'lang/env';
 
 export const takeBinding = new PublicBinding(
   "take",
-  ``,
+
+  `invokes the 'from' function 'num' times, returning a list`,
+
   () => function({num = 0, from = () => 0}) {
     let res = [];
     for(let i=0; i<num; i++) {
