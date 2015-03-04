7dd61893294a1a601d93412fec6fbad34dea21f0
Inderjit Gill <email@indy.io>
2015-03-04 10:22:48 +0000

    updated packages and made 'gulp test' work

--- test/setup/node.js
+++ test/setup/node.js
@@ -2,5 +2,10 @@ global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
 
+global.Immutable = require('immutable');
+global.glmatrix = require('gl-matrix');
+global.mat4 = global.glmatrix.mat4;
+global.vec3 = global.glmatrix.vec3;
+
 require('babel/register');
 require('./setup')();
