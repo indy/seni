219dab8771c4b28b7263290eebf378fb6529a2a0
Inderjit Gill <email@indy.io>
2015-03-18 21:14:12 +0000

    runtime using const

--- src/lang/Runtime.js
+++ src/lang/Runtime.js
@@ -4,34 +4,32 @@ import Lexer from './Lexer';
 import Compiler from './Compiler';
 import Genetic from './Genetic';
 
-let Runtime = {
+const Runtime = {
   createEnv: function() {
     return Interpreter.getBasicEnv();
   },
 
   evalForm: function(env, form) {
-    let tokensBox = Lexer.tokenise(form);
+    const tokensBox = Lexer.tokenise(form);
     if(tokensBox.error) {
       console.log(tokensBox.error);
       return false;
     }
-    let astBox = Parser.parse(tokensBox.tokens);
+    const astBox = Parser.parse(tokensBox.tokens);
     if(astBox.error) {
       // some sort of error occurred
       console.log(astBox.error);
       return false;
     }
 
-    let ast = astBox.nodes;
+    const ast = astBox.nodes;
 
     const traits = Genetic.buildTraits(ast);
     const genotype = Genetic.createGenotypeFromInitialValues(traits);
-    let compiled = Compiler.compile(ast, genotype);
-
-    console.log(compiled);
+    const compiled = Compiler.compile(ast, genotype);
 
     // add all of the define expressions to the env
-    let [_env, _res] = compiled.forms.
+    const [_env, _res] = compiled.forms.
           filter(Interpreter.isDefineExpression).
           reduce(([e, r], b) => Interpreter.evaluate(e, b), [env, false]);
 
