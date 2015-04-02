ff125efab1001779c60d8eb829c6dac04784598e
Inderjit Gill <email@indy.io>
2015-04-02 11:27:52 +0100

    using the npm packaged version of seedrandom

--- test/setup/node.js
+++ test/setup/node.js
@@ -3,6 +3,8 @@ global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
 
 global.Immutable = require('immutable');
+global.Math.seedrandom = require('seedrandom');
+
 global.glmatrix = require('gl-matrix');
 global.mat4 = global.glmatrix.mat4;
 global.vec3 = global.glmatrix.vec3;
