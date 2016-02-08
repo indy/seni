58ff662f29c3dd78ad261c5d2a02f2fa31f9f4aa
Inderjit Gill <email@indy.io>
2016-02-08 22:09:47 +0000

    161g-grid-flow added to gallery

--- server/server.js
+++ server/server.js
@@ -46,7 +46,8 @@ function buildGalleryData() {
                ['15he-cos-1', '15he-cos-1.png'],
                ['15he-cos-2', '15he-cos-2.png'],
                ['15he-cos-3', '15he-cos-3.png'],
-               ['160c-schotter', '160c-schotter.png']
+               ['160c-schotter', '160c-schotter.png'],
+               ['161g-grid-flow', '161g-grid-flow.png']
               ];
 
   var res = [];
