a742ec0c94c67b6695c9c8e2184a212fc57b0c21
Inderjit Gill <email@indy.io>
2017-03-17 19:35:07 +0000

    main_wasm -> wasm

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,4 +1,4 @@
 mkdir build_wasm
 pushd build_wasm
-emcc -o seni-wasm.html ../code/main_wasm.c ../code/seni.c -O3 -s WASM=1 --shell-file ../misc/html_template/shell_minimal.html -DPLATFORM_WASM
+emcc -o seni-wasm.html ../code/wasm.c ../code/seni.c -O3 -s WASM=1 --shell-file ../misc/html_template/shell_minimal.html -DPLATFORM_WASM
 popd
