e7d2777f184d578e154bedba1e6b253b7c55b9cd
Inderjit Gill <email@indy.io>
2017-03-31 12:13:21 +0100

    started interpreter

---
+++ src/seni_lang_interpreter.c
@@ -0,0 +1,6 @@
+#include "seni_lang_interpreter.h"
+
+seni_val *evaluate(seni_env *env, seni_node *ast)
+{
+  
+}
---
+++ src/seni_lang_interpreter.h
@@ -0,0 +1,6 @@
+#ifndef SENI_LANG_INTERPRETER_H
+#define SENI_LANG_INTERPRETER_H
+
+seni_val *evaluate(seni_env *env, seni_node *ast);
+
+#endif
