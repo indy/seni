aec57644fc15bf81dc02dfda62765e2d110f39e1
Inderjit Gill <email@indy.io>
2017-05-04 22:17:29 +0100

    opcodes for jumping

--- app/c/seni_opcodes.h
+++ app/c/seni_opcodes.h
@@ -1,7 +1,3 @@
-// label
-// goto
-// if-goto
-
 // function
 // call
 // return
@@ -17,10 +13,17 @@ OPCODE(EQ, -1)
 OPCODE(GT, -1)
 OPCODE(LT, -1)
 
-
 OPCODE(AND, -1)
 OPCODE(OR, -1)
 OPCODE(NOT, 0)
+
+// Jump the instruction pointer [arg] forward.
+OPCODE(JUMP, 0)
+// Jump the instruction pointer [arg] backward.
+OPCODE(LOOP, 0)
+// Pop and if not truthy then jump the instruction pointer [arg] forward.
+OPCODE(JUMP_IF, -1)
+
 
 OPCODE (NOP, 0)
 OPCODE (STOP, 0)
