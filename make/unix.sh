9a80823dc15aa4374ddbd235daf34b3cb6f7f48e
Inderjit Gill <email@indy.io>
2017-05-07 16:20:24 +0100

    unix binary in build_unix not build_osx

--- make/unix.sh
+++ make/unix.sh
@@ -9,14 +9,14 @@ if [ "$1" == "wasm" ]; then
 else
 
     # build for the native platform and run tests
-    mkdir build_osx
-    pushd build_osx    
+    mkdir build_unix
+    pushd build_unix    
     # cc -o test -std=c99 -Wall -Wextra ../app/c/test.c ../app/c/unity/unity.c ../app/c/gl-matrix/*.c ../app/c/seni.c ../app/c/seni_*.c
     cc -o test -std=c99 ../app/c/test.c ../app/c/unity/unity.c ../app/c/gl-matrix/*.c ../app/c/seni.c ../app/c/seni_*.c -lm
     popd
 
     if [ "$1" == "test" ]; then
-        ./build_osx/test
+        ./build_unix/test
     fi
 
 fi
