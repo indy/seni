40bce809a2366cb2a311fb03672f8b9f358899b7
Inderjit Gill <email@indy.io>
2017-06-06 17:12:37 +0100

    nologo when invoking Microsoft compiler

--- make/win.bat
+++ make/win.bat
@@ -20,7 +20,7 @@ if "%1" == "wasm" (
       set test_sources=..\app\c\test.c ..\app\c\unity\unity.c ..\app\c\seni.c ..\app\c\seni_*.c
       
       rem https://docs.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-alphabetically
-      cl /W4 /wd4127 /wd4001 -Zi -Za /D_CRT_SECURE_NO_DEPRECATE /TC !test_sources! /link /OUT:test.exe
+      cl /nologo /W4 /wd4127 /wd4001 -Zi -Za /D_CRT_SECURE_NO_DEPRECATE /TC !test_sources! /link /OUT:test.exe
   popd
 
   if "%1" == "test" (
