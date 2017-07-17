c44d6b95f1f4b79936a016ebb991cab616d02b84
Inderjit Gill <email@indy.io>
2017-07-17 22:14:39 +0100

    TINY_FLOAT -> SENI_EPSILON

--- app/c/seni_focal.c
+++ app/c/seni_focal.c
@@ -5,13 +5,13 @@
 #include "seni_keyword_iname.h"
 
 // TODO: find out how small this can be and place it somewhere more accessible for other code
-#define TINY_FLOAT 0.000001f
+#define SENI_EPSILON 0.000001f
 
 f32 focal_point(f32 x, f32 y, f32 distance, i32 mapping, f32 centre_x, f32 centre_y)
 {
   f32 d = distance_v2(x, y, centre_x, centre_y);
 
-  if (d < TINY_FLOAT) {
+  if (d < SENI_EPSILON) {
     return 1.0f;
   }
 
@@ -25,7 +25,7 @@ f32 focal_hline(f32 y, f32 distance, i32 mapping, f32 centre_y)
   f32 d = centre_y - y;
   d = absf(d);
 
-  if (d < TINY_FLOAT) {
+  if (d < SENI_EPSILON) {
     return 1.0f;
   }
 
@@ -39,7 +39,7 @@ f32 focal_vline(f32 x, f32 distance, i32 mapping, f32 centre_x)
   f32 d = centre_x - x;
   d = absf(d);
 
-  if (d < TINY_FLOAT) {
+  if (d < SENI_EPSILON) {
     return 1.0f;
   }
 
