d61757adc0524977a7a2bcfeb175bdc7194beb3a
Inderjit Gill <email@indy.io>
2018-05-11 21:36:06 +0100

    updating dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,34 @@
 //var webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: {
-    sen: ['./src/js/index.js'],
-    worker: ['./src/js/worker.js'],
-    piece: ['./src/js/piece.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[id].bundle.js',
-    sourceMapFilename: '[file].map'
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
+module.exports = [
+  {
+    entry: {
+      sen: ['./src/js/index.js'],
+      worker: ['./src/js/worker.js'],
+      piece: ['./src/js/piece.js']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      chunkFilename: '[id].bundle.js',
+      sourceMapFilename: '[file].map'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
 
-        // Only run `.js` files through Babel
-        test: /\.js$/,
+          // Only run `.js` files through Babel
+          test: /\.js$/,
 
-        // Skip any files outside of your project's `js` directory
-        include: [
-          path.resolve(__dirname, 'src', 'js')
-        ]
-      }
-    ]
-  },
-  devtool: 'source-map'
-};
+          // Skip any files outside of your project's `js` directory
+          include: [
+            path.resolve(__dirname, 'src', 'js')
+          ]
+        }
+      ]
+    },
+    devtool: 'source-map'
+  }
+];
