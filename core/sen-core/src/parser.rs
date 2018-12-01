653b91c1bfc5f6f6585372ba8fec5284cbe2349a
Inderjit Gill <email@indy.io>
2018-12-01 22:14:09 +0000

    wip: parser

--- core/sen-core/src/parser.rs
+++ core/sen-core/src/parser.rs
@@ -13,61 +13,160 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use lexer::Token;
-
+use error::{SenResult, SenError};
+use lexer::{Token, tokenize};
 
 struct Gene {
     this_is_a_placeholder: i32,
 }
 
-struct NodeMeta<'a> {
+struct NodeMeta {
     gene: Option<Gene>,
-    parameter_ast: Vec<Node<'a>>,
-    parameter_prefix: Vec<Node<'a>>,
+    parameter_ast: Vec<Node>,
+    parameter_prefix: Vec<Node>,
 }
 
-#[derive(Debug, Eq, PartialEq)]
-pub enum Node<'a> {
-    List(Vec<Node<'a>>),
-    Vector(Vec<Node<'a>>),
-    Int(&'a str),
-    Float(&'a str),
-    Name(&'a str),
-    Label(&'a str),
-    String(&'a str),
-    Whitespace(&'a str),
-    Comment(&'a str),
-    Fake,
+#[derive(Debug, PartialEq)]
+pub enum Node {
+    List(Vec<Node>),
+    Vector(Vec<Node>),
+    Int(String),
+    Float(f32),
+    Name(String),
+    Label(String),
+    String(String),
+    Whitespace(String),
+    Comment(String),
 }
 
+struct NodeAndRemainder<'a> {
+    node: Node,
+    tokens: &'a [Token<'a>],
+}
 
-pub fn parse(tokens: Vec<Token>) -> Vec<Node> {
+// At the first token after a Token::ParenStart
+//
+fn eat_list<'a>(t: &'a [Token<'a>]) -> SenResult<NodeAndRemainder<'a>> {
+    let mut tokens = t;
+    let mut res: Vec<Node> = Vec::new();
+
+    loop {
+        match tokens[0] {
+            Token::ParenEnd => return Ok(NodeAndRemainder {
+                node: Node::List(res),
+                tokens: &tokens[1..],
+            }),
+            _ => {
+                match eat_token(tokens) {
+                    Ok(nar) => {
+                        res.push(nar.node);
+                        tokens = nar.tokens;
+                    },
+                    Err(e) => return Err(e)
+                }
+            }
+        }
+    }
+}
+
+fn eat_token<'a>(tokens: &'a[Token<'a>]) -> SenResult<NodeAndRemainder<'a>> {
+    match tokens[0] {
+        Token::Name(txt) =>
+            if tokens.len() > 1 && tokens[1] == Token::Colon {
+                Ok(NodeAndRemainder {
+                    node: Node::Label(txt.to_string()),
+                    tokens: &tokens[2..],
+                })
+            } else {
+                Ok(NodeAndRemainder {
+                    node: Node::Name(txt.to_string()),
+                    tokens: &tokens[1..],
+                })
+            },
+        Token::Number(txt) => Ok(NodeAndRemainder {
+            node: Node::Float(txt.parse().unwrap()), // todo: fix unwrap
+            tokens: &tokens[1..],
+        }),
+        Token::Whitespace(ws) => Ok(NodeAndRemainder {
+            node: Node::Whitespace(ws.to_string()),
+            tokens: &tokens[1..],
+        }),
+        Token::Comment(comment) => Ok(NodeAndRemainder {
+            node: Node::Comment(comment.to_string()),
+            tokens: &tokens[1..],
+        }),
+        Token::ParenStart => eat_list(&tokens[1..]),
+        _ => Err(SenError::GeneralError),
+    }
+
+}
+
+pub fn parse(s: &str) -> SenResult<Vec<Node>> {
+    let t = tokenize(s)?;
+
+    let mut tokens = t.as_slice();
     let mut res = Vec::new();
 
-    let tok = tokens[0];
-    let node = match tok {
-        Token::Name(n) => Node::Name(n),
-        _ => Node::Fake
-    };
+    while tokens.len() > 0 {
+        match eat_token(tokens) {
+            Ok(nar) => {
+                res.push(nar.node);
+                tokens = nar.tokens;
+            },
+            Err(e) => return Err(e)
+        }
+    }
 
-    res.push(node);
-
-    res
+    Ok(res)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use lexer::tokenize;
 
     fn ast(s: &str) -> Vec<Node> {
-        let t = tokenize(s).unwrap();
-        let n = parse(t);
-        n
+        parse(s).unwrap()
     }
 
     #[test]
     fn test_parser() {
-        assert_eq!(ast("hello"), [Node::Name("hello")])
+        assert_eq!(ast("hello"),
+                   [Node::Name("hello".to_string())]);
+        assert_eq!(ast("hello world"),
+                   [Node::Name("hello".to_string()),
+                    Node::Whitespace(" ".to_string()),
+                    Node::Name("world".to_string())]);
+        assert_eq!(ast("hello: world"),
+                   [Node::Label("hello".to_string()),
+                    Node::Whitespace(" ".to_string()),
+                    Node::Name("world".to_string())]);
+        assert_eq!(ast("42 102"),
+                   [Node::Float(42.0),
+                    Node::Whitespace(" ".to_string()),
+                    Node::Float(102.0)]);
+
+        assert_eq!(ast("hello world ; some comment"),
+                   [Node::Name("hello".to_string()),
+                    Node::Whitespace(" ".to_string()),
+                    Node::Name("world".to_string()),
+                    Node::Whitespace(" ".to_string()),
+                    Node::Comment(" some comment".to_string())]);
+
+        assert_eq!(ast("(hello world)"),
+                   [Node::List(vec![Node::Name("hello".to_string()),
+                                    Node::Whitespace(" ".to_string()),
+                                    Node::Name("world".to_string())])]);
+
+        assert_eq!(ast("(hello world (1 2 3))"),
+                   [Node::List(vec![Node::Name("hello".to_string()),
+                                    Node::Whitespace(" ".to_string()),
+                                    Node::Name("world".to_string()),
+                                    Node::Whitespace(" ".to_string()),
+                                    Node::List(vec![Node::Float(1.0),
+                                                    Node::Whitespace(" ".to_string()),
+                                                    Node::Float(2.0),
+                                                    Node::Whitespace(" ".to_string()),
+                                                    Node::Float(3.0)])])]);
+
     }
 }
