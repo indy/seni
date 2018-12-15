f01cb5872750a2253e0900460e1c3c86b7255944
Inderjit Gill <email@indy.io>
2018-12-15 10:30:16 +0000

    native/make_unix builds in the dist/ folder

--- native/make_unix.sh
+++ native/make_unix.sh
@@ -2,5 +2,5 @@
 
 # -g flag is for producing debug information
 # paths are relative to dist directory
-cc -o native -std=c99 src/native.c ../core/src/seni/*.c -lm -O2 -g -I ../core/src
-./native $2
+cc -o dist/native -std=c99 src/native.c ../core/src/seni/*.c -lm -O2 -g -I ../core/src
+./dist/native $2
