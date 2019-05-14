6606ee3c1f46adb8966271b6de3c0550dc208511
Inderjit Gill <email@indy.io>
2019-05-14 22:18:25 +0100

    larger RENDER_PACKET_MAX_SIZE

--- core/src/geometry/mod.rs
+++ core/src/geometry/mod.rs
@@ -27,9 +27,10 @@ pub mod rect;
 pub mod stroked_bezier;
 pub mod stroked_bezier_rect;
 
-// todo: work out reasonable defaults
-const RENDER_PACKET_MAX_SIZE: usize = 40096;
+const RENDER_PACKET_MAX_SIZE: usize = 262144;
 pub const RENDER_PACKET_FLOAT_PER_VERTEX: usize = 8;
+// 262144 * 4 == 1MB per render packet
+// 262144 / 8 == 32768 vertices per render packet
 
 pub struct RenderPacket {
     pub geo: Vec<f32>,
