25fe0af574740320d5333acb8271a6fc0f72c9dd
Inderjit Gill <email@indy.io>
2019-05-15 18:50:08 +0100

    client builds release by default

--- client/build.bat
+++ client/build.bat
@@ -2,10 +2,12 @@
 
 setlocal EnableDelayedExpansion
 
-if "%1" == "release" (
-    cargo build --release --target wasm32-unknown-unknown
-    wasm-bindgen target/wasm32-unknown-unknown/release/client.wasm --out-dir www --no-typescript --no-modules
-) else (
+rem builds a release build by default
+
+if "%1" == "debug" (
     cargo build --target wasm32-unknown-unknown
     wasm-bindgen target/wasm32-unknown-unknown/debug/client.wasm --out-dir www --no-typescript --no-modules
+) else (
+    cargo build --release --target wasm32-unknown-unknown
+    wasm-bindgen target/wasm32-unknown-unknown/release/client.wasm --out-dir www --no-typescript --no-modules
 )
--- client/build.sh
+++ client/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if [ "$1" = "release" ]
+# builds a release build by default
+
+if [ "$1" = "debug" ]
 then
-    cargo build --release --target wasm32-unknown-unknown
-    wasm-bindgen target/wasm32-unknown-unknown/release/client.wasm --out-dir www --no-typescript --no-modules
-else
     cargo build --target wasm32-unknown-unknown
     wasm-bindgen target/wasm32-unknown-unknown/debug/client.wasm --out-dir www --no-typescript --no-modules
+else
+    cargo build --release --target wasm32-unknown-unknown
+    wasm-bindgen target/wasm32-unknown-unknown/release/client.wasm --out-dir www --no-typescript --no-modules
 fi
