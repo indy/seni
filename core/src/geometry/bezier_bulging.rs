0918a2d01522be915dd65393faaa40c43e31b184
Inderjit Gill <email@indy.io>
2020-01-08 21:23:52 +0000

    updated copyright notice to 2020

--- core/src/geometry/bezier_bulging.rs
+++ core/src/geometry/bezier_bulging.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Inderjit Gill
+// Copyright (C) 2020 Inderjit Gill <email@indy.io>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
