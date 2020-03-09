ee005d6de69d543060ff7091d97fa94513ded32b
Inderjit Gill <email@indy.io>
2020-03-09 12:07:46 +0000

    fixed copyright notices

--- core/src/render_packet.rs
+++ core/src/render_packet.rs
@@ -1,11 +1,13 @@
 // Copyright (C) 2020 Inderjit Gill <email@indy.io>
 
-// This program is free software: you can redistribute it and/or modify
+// This file is part of Seni
+
+// Seni is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful,
+// Seni is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
